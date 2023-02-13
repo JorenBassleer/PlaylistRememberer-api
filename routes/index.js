635f711f911b8e002cd01b5c6934a9e5065030d8
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-02-13 20:48:02 +0100

    further basic structure

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const { Router } = require('express');
+
+const video = require('./video');
+const playlist = require('./playlist');
+
+module.exports = () => {
+  const app = Router();
+  video(app);
+  playlist(app);
+};
