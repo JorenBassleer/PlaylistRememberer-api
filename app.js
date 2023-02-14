5abf648187cbbd86a4f25b4a6346c7343ac95432
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-02-14 20:23:28 +0100

    further general setup

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const dbURI = process.env.dburi;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+});
+app.use('/', routes());
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(PORT);
+  })
+  .catch(() => console.log('err'));
+  
