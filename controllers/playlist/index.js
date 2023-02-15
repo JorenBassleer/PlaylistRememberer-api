91eee7eed666ede7cfb764a092d6f85866a289e2
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-02-15 20:50:26 +0100

    added playlist controller basic funcs

---
+++ controllers/playlist/index.js
@@ -0,0 +1,28 @@
+const Playlist = require('../../models/playlist');
+
+const getAllPlaylists = async () => {
+
+};
+
+const findPlaylistById = async () => {
+
+};
+
+const createPlaylist = async () => {
+
+};
+
+const updatePlaylistById = async () => {
+
+};
+
+const deletePlaylistById = async () => {
+
+};
+module.exports = {
+  getAllPlaylists,
+  findPlaylistById,
+  createPlaylist,
+  updatePlaylistById,
+  deletePlaylistById,
+}
