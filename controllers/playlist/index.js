681371b3181dc19f0f1048575b9963563a390007
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-02-15 20:51:49 +0100

    added basic video controller funcs

--- controllers/playlist/index.js
+++ controllers/playlist/index.js
@@ -25,4 +25,4 @@ module.exports = {
   createPlaylist,
   updatePlaylistById,
   deletePlaylistById,
-}
+};
---
+++ controllers/video/index.js
@@ -0,0 +1,28 @@
+const Video = require('../../models/video');
+
+const getAllVideos = async () => {
+
+};
+
+const findVideoById = async () => {
+
+};
+
+const createVideo = async () => {
+
+};
+
+const updateVideoById = async () => {
+
+};
+
+const deleteVideoById = async () => {
+
+};
+module.exports = {
+  getAllVideos,
+  findVideoById,
+  createVideo,
+  updateVideoById,
+  deleteVideoById,
+};
