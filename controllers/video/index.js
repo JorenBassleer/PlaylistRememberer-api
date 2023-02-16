a7527ee3ca8a88184c9c9cf1020aaee87f13f61a
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-02-16 20:16:46 +0100

    added video controller funcs

--- controllers/video/index.js
+++ controllers/video/index.js
@@ -1,24 +1,40 @@
 const Video = require('../../models/video');
 
 const getAllVideos = async () => {
-
+  const allVideos = await Video.find({});
+  return allVideos;
 };
 
-const findVideoById = async () => {
-
+const findVideoById = async (id) => {
+  const foundVideo = await Video.findById({ _id: id });
+  if (!foundVideo) throw new Error('No video with that id');
+  return foundVideo;
 };
 
-const createVideo = async () => {
-
+const createVideo = async (video) => {
+  const newVideo = await (await Video.create(video)).save();
+  return newVideo;
 };
 
-const updateVideoById = async () => {
+const updateVideoById = async (id, video) => {
+  const updatedVideo = await Video.findByIdAndUpdate(
+    id,
+    { $set: video },
+    // eslint-disable-next-line comma-dangle
+    {
+      lean: true,
+      new: true,
+    },
+  );
 
+  return updatedVideo;
 };
 
-const deleteVideoById = async () => {
-
+const deleteVideoById = async (id) => {
+  const deletedVideo = await Video.deleteOne({ _id: id });
+  return deletedVideo;
 };
+
 module.exports = {
   getAllVideos,
   findVideoById,
