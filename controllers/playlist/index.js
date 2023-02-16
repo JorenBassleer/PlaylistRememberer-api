046b4dab4eec2f57608076b41cfb405a70bd2262
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-02-16 20:21:13 +0100

    added playlist controller funcs

--- controllers/playlist/index.js
+++ controllers/playlist/index.js
@@ -1,23 +1,38 @@
 const Playlist = require('../../models/playlist');
 
 const getAllPlaylists = async () => {
-
+  const allPlalist = await Playlist.find({});
+  return allPlalist;
 };
 
-const findPlaylistById = async () => {
-
+const findPlaylistById = async (id) => {
+  const foundPlaylist = await Playlist.findById({ _id: id });
+  if (!foundPlaylist) throw new Error('No playlist with that id');
+  return foundPlaylist;
 };
 
-const createPlaylist = async () => {
-
+const createPlaylist = async (playlist) => {
+  const newPlaylist = await (await Playlist.create(playlist)).save();
+  return newPlaylist;
 };
 
-const updatePlaylistById = async () => {
+const updatePlaylistById = async (id, playlist) => {
+  const updatedPlaylist = await Playlist.findByIdAndUpdate(
+    id,
+    { $set: playlist },
+    // eslint-disable-next-line comma-dangle
+    {
+      lean: true,
+      new: true,
+    },
+  );
 
+  return updatedPlaylist;
 };
 
-const deletePlaylistById = async () => {
-
+const deletePlaylistById = async (id) => {
+  const deletedPlaylist = await Playlist.deleteOne({ _id: id });
+  return deletedPlaylist;
 };
 module.exports = {
   getAllPlaylists,
