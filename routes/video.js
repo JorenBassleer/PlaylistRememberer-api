02deca6fa9fa41a599183df177de28d8bba0580f
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-02-20 20:32:00 +0100

    added route funcs for video

--- routes/video.js
+++ routes/video.js
@@ -5,19 +5,44 @@ const videoController = require('../controllers/video');
 
 module.exports = (app) => {
   app.use('/video', router);
-  router.get('/', (req, res) => {
-
+  router.get('/', async (req, res) => {
+    try {
+      const videoRecords = await videoController.getAllVideos();
+      return res.status(200).json(videoRecords);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
-  router.post('/', (req, res) => {
-
+  router.post('/', async (req, res) => {
+    try {
+      const createdVideo = await videoController.createVideo(req.body);
+      return res.status(200).json(createdVideo);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
-  router.get('/:id', (req, res) => {
-
+  router.get('/:id', async (req, res) => {
+    try {
+      const foundVideo = await videoController.findVideoById(req.params.id);
+      return res.status(200).json(foundVideo);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
-  router.put('/:id', (req, res) => {
-
+  router.put('/:id', async (req, res) => {
+    try {
+      const updatedVideo = await videoController.updateVideoById(req.params.id, req.body);
+      return res.status(200).json(updatedVideo);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
-  router.delete('/:id', (req, res) => {
-
+  router.delete('/:id', async (req, res) => {
+    try {
+      const deletedVideo = await videoController.deleteVideoById(req.params.id);
+      return res.status(200).json(deletedVideo);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
 };
