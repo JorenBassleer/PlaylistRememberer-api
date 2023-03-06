6c7970341e42b6d96e5b699aa0488432761c4901
Joren Bassleer <jorenbassleer1997@gmail.com>
2023-03-06 21:07:27 +0100

    updated settings & setup & added playlist routes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config();
 const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const dbURI = process.env.dburi;
+const { MONGODB_URI } = process.env;
 
 app.use(cors());
 app.use(express.json());
@@ -14,7 +15,7 @@ app.use(express.urlencoded({
   extended: true,
 }));
 app.use('/', routes());
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(PORT);
   })
--- routes/playlist.js
+++ routes/playlist.js
@@ -5,23 +5,45 @@ const playlistController = require('../controllers/playlist');
 
 module.exports = (app) => {
   app.use('/playlist', router);
-  router.get('/', (req, res) => {
+  router.get('/', async (req, res) => {
+    console.log('test');
     try {
-
+      const playlsitRecords = await playlistController.getAllPlaylists();
+      return res.status(200).json(playlsitRecords);
     } catch (error) {
-
+      return res.status(500).json(error);
     }
   });
-  router.post('/', (req, res) => {
-
+  router.post('/', async (req, res) => {
+    try {
+      const createdPlaylist = await playlistController.createPlaylist(req.body);
+      return res.status(200).json(createdPlaylist);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
-  router.get('/:id', (req, res) => {
-
+  router.get('/:id', async (req, res) => {
+    try {
+      const foundPlaylist = await playlistController.findPlaylistById(req.params.id);
+      return res.status(200).json(foundPlaylist);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
-  router.put('/:id', (req, res) => {
-
+  router.put('/:id', async (req, res) => {
+    try {
+      const updatedPlaylist = await playlistController.updatePlaylistById(req.params.id, req.body);
+      return res.status(200).json(updatedPlaylist);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
-  router.delete('/:id', (req, res) => {
-
+  router.delete('/:id', async (req, res) => {
+    try {
+      const deletedPlaylist = await playlistController.deletePlaylistById(req.params.id);
+      return res.status(200).json(deletedPlaylist);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   });
 };
