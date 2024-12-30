6d4d5cc1b0657f1ab358553f9c005e05fd8ea277
Joren Bassleer <jorenbassleer1997@gmail.com>
2024-12-30 21:16:47 +0100

    somewhat progress in auth

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,13 @@ module.exports = {
     'vue',
   ],
   rules: {
+    'max-len': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'error',
+    'linebreak-style': 'off',
+    'quote-props': ['error', 'consistent-as-needed'],
+    'prefer-arrow-callback': ['error', { allowNamedFunctions: true }],
+    'func-style': ['error', 'expression', { allowArrowFunctions: true }],
+    'array-bracket-newline': ['error', { multiline: true }],
   },
 };
--- routes/google.js
+++ routes/google.js
@@ -1,14 +1,36 @@
 const express = require('express');
+const { google } = require('googleapis');
+const passport = require('passport');
+
+const crypto = require('crypto');
+const secrets = require('../client_secret.json');
 
 const router = express.Router();
-const passport = require('passport');
+
+const oauth2Client = new google.auth.OAuth2(
+  secrets.web.client_id,
+  secrets.web.client_secret,
+  secrets.web.redirect_uris[0],
+);
+
+const scopes = ['https://www.googleapis.com/auth/youtube.force-ssl'];
 
 module.exports = (app) => {
   app.use('/google', router);
+  // https://developers.google.com/youtube/v3/guides/auth/server-side-web-apps#node.js
   router.get('/', async (req, res) => {
     try {
-      passport.authenticate('google', { scope: 'profile' });
-      return res.status(200);
+      const state = crypto.randomBytes(32).toString('hex');
+
+      req.session.state = state;
+
+      const authorizationUrl = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes,
+        include_granted_scopes: true,
+        state,
+      });
+      return res.status(200).json(authorizationUrl);
     } catch (err) {
       return res.status(500).json(err);
     }
